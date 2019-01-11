4a5e97cbbca4c4825b51f261dd4441ca94167b6b
Danielle <danielle.sackstein94@gmail.com>
2019-01-11 21:33:32 +0200

    before with abba

--- TMM/GetReference.py
+++ TMM/GetReference.py
@@ -7,7 +7,6 @@ class GetReference:
     def __init__(self, dir):
         self.dir = dir
         self.files = np.array([]).astype(FileWrap)
-        # self.genes_matrix = np.array([])
         self.quintiles = np.array([])
         self.get_library_files(dir)
         self.count_read_scaled_counts()
@@ -19,38 +18,34 @@ class GetReference:
                 continue
             if file_name.endswith(".genes.results"):
                 file = open(full_path, "r")
-                self.files = np.append(self.files, np.array([FileWrap(file, file_name)]))
+                self.files = np.append(self.files, np.array([FileWrap(file, file_name, dir_name)]))
         self.files = np.sort(self.files)
 
     def get_quintile(self, genes_count):
         sorted_genes_count = np.sort(genes_count)
-        return sorted_genes_count[math.floor((0.75) * sorted_genes_count.shape[0])-1]
+        return sorted_genes_count[math.floor((0.75) * sorted_genes_count.shape[0]) - 1]
 
     def count_read_scaled_counts(self):
         for i, file in enumerate(self.files):
             if file.file_name.endswith(".genes.results"):
-                genes_count = np.loadtxt(self.dir + "\\" + file.file_name, dtype=str, delimiter="\t")[1:, 4].astype(np.float)
+                genes_count = np.loadtxt(file.file_name, dtype=str, delimiter="\t")[1:, 4].astype(
+                    np.float)
                 col_sum = np.sum(genes_count)
                 if col_sum > 0:
                     genes_count /= col_sum
-                # if i == 0:
-                    # self.genes_matrix = genes_count
-                # else:
-                    # self.genes_matrix = np.vstack((self.genes_matrix, genes_count))
                 self.quintiles = np.append(self.quintiles, self.get_quintile(genes_count))
+
         quintiles_average = np.average(self.quintiles)
         diff = np.abs(quintiles_average - self.quintiles)
         min_diff = np.argmin(diff)
-        # print(self.genes_matrix)
-        print(self.files[min_diff].file_name)
-        return self.files[min_diff].file
+        return self.files[min_diff], self.files
 
 
 class FileWrap:
 
-    def __init__(self, file, file_name):
+    def __init__(self, file, file_name, dir):
         self.file = file
-        self.file_name = file_name
+        self.file_name = dir + "\\" + file_name
         self.file_index = int(file_name.split(".")[1])
 
     def __gt__(self, other):
--- TMM/TMM_Updated.py
+++ TMM/TMM_Updated.py
@@ -1,116 +1,154 @@
-import math
 import sys
-import pandas as pd
 import numpy as np
 
+from GetReference import GetReference
 
-def write_to_file(N_k_1, N_k_2, Y_k_1, Y_k_2, first_library_index, genes, second_library_index):
-    out_file = open("libraries.{}.and.{}.with.log.results".format(first_library_index, second_library_index), "w")
-    out_file.write("gene_id\texpected_count_library_{}\texpected_count_library_{}\n".format(first_library_index,
-                                                                                            second_library_index))
+NUM_OF_REPEATS = 4
+
+
+def write_to_file_two_conditions_one_repeat(reference, new_sample, genes, reference_index, sample_index, input_dir):
+    out_file = open(input_dir + "\\test\\" + "new.libraries.{}.and.{}.results".format(reference_index, sample_index),
+                    "w")
+    out_file.write("gene_id\texpected_reference_{}\texpected_count_library_{}\n".format(reference_index,
+                                                                                        sample_index))
     print("Writing to file....")
-    for i in range(1, Y_k_1.shape[0]):
-        print(i)
+    count = 0
+    for i in range(reference.shape[0]):
         out_file.write(genes[i] + "\t")
-        out_file.write(str(float(Y_k_1[4][i]) / N_k_1) + "\t")
-        out_file.write(str(float(Y_k_2[4][i]) / N_k_2) + "\n")
+        out_file.write(str(float(reference[i])) + "\t")
+        out_file.write(str(float(new_sample[i])) + "\n")
+    print("count:" + str(count))
 
 
-def get_M_g_v(N_k_1, N_k_2, Y_k_1, Y_k_2):
-    M_g_v = np.zeros((Y_k_1.shape[0], 1))
+def write_to_file_four_repeats(input_dir, normalized_samples, genes):
+    for first_condition in range(7):
+        for second_condition in range(first_condition + 1, 7):
+            out_file = open(
+                input_dir + "\\test\\" + "compare.condition.{}.and.condition{}.results".format(first_condition,
+                                                                                               second_condition), "w")
+            out_file.write(
+                "gene_id\texpected_count_with_all_repeats_condition_{}\texpected_count_with_all_repeats_condition_{}\n".format(
+                    first_condition,
+                    second_condition))
 
-    for i in range(1, Y_k_1.shape[0]):
-        Y_g_k_1 = float(Y_k_1[4][i])
-        Y_g_k_2 = float(Y_k_2[4][i])
+            first_cond_repeats = np.zeros((NUM_OF_REPEATS, 0))
+            for library in range(1, 29):
+                if library % NUM_OF_REPEATS == first_condition:
+                    first_cond_repeats = np.append(first_cond_repeats, library)
 
-        if Y_g_k_1 == 0 or Y_g_k_2 == 0:
-            M_g = 0
+            second_cond_repeats = np.zeros((NUM_OF_REPEATS, 0))
+            for library in range(1, 29):
+                if library % NUM_OF_REPEATS == first_condition:
+                    second_cond_repeats = np.append(second_cond_repeats, library)
 
-        else:
-            # M_g = np.log(Y_g_k_1 / N_k_1) / np.log(Y_g_k_2 / N_k_2)
-            M_g = np.log((Y_g_k_1 / N_k_1) / (Y_g_k_2 / N_k_2))
-            W_g = (N_k_1 - Y_g_k_1) / (N_k_1 * Y_g_k_1) + (N_k_2 - Y_g_k_2) / (N_k_2 * Y_g_k_2)
-            M_g = M_g * W_g
-
-        M_g_v[i] = M_g
-
-    return M_g_v
-
-
-def get_files(input_file_1, input_file_2):
-    file_1 = pd.read_csv(input_file_1, header=None, sep="\t")
-    file_2 = pd.read_csv(input_file_2, header=None, sep="\t")
-
-    df_file_1 = pd.DataFrame(file_1)
-    df_file_2 = pd.DataFrame(file_2)
-
-    return df_file_1, df_file_2
+            for gene_count in range(genes.shape[0]):
+                out_file.write(genes[gene_count] + "\t")
+                for repeat_index in range(NUM_OF_REPEATS):
+                    out_file.write(str(float(normalized_samples[first_cond_repeats[repeat_index]][gene_count] + "\t")))
+                    out_file.write(str(float(normalized_samples[second_cond_repeats[repeat_index]][gene_count] + "\t")))
 
 
-def trim(M_g_v):
-    M_g_v_sort = np.sort(M_g_v, axis=None)
-    M_g_v_sort_without_zeros = M_g_v_sort[np.nonzero(M_g_v_sort)]
-    n = math.floor(0.7 * M_g_v_sort_without_zeros.shape[0])
-    return M_g_v_sort_without_zeros[:n]
+def calculate_weights_and_factor(log_reference_sample, geo_mean_of_logs):
+    sum = np.sum(geo_mean_of_logs)
+    weights = np.zeros((geo_mean_of_logs.shape[0],))
+    if sum != 0:
+        weights = geo_mean_of_logs / np.sum(geo_mean_of_logs)
+    scale_factor = np.sum(np.dot(log_reference_sample, weights))
+    scale_factor = np.power(2, scale_factor)
+    return scale_factor
 
 
-def sum_vector(Y_k):
-    sum = 0
-    for i in range(1, Y_k.shape[0] - 1):
-        sum += float(Y_k[4][i])
-    return sum
+def find_indeces(x, bottom, top):
+    q1 = np.quantile(x, bottom)
+    q2 = np.quantile(x, top)
+
+    c1 = np.where(q1 < x)
+    c2 = np.where(x < q2)
+    c3 = np.where(x != float("-inf"))
+    c4 = np.intersect1d(c1, c2)
+    d = np.intersect1d(c4, c3)
+    return d
 
 
-def get_sum_M_g_v_f(M_g_v_f):
-    sum = 0
-    for i in range(len(M_g_v_f)):
-        sum += M_g_v_f[i]
-    return sum
+def get_vectors_at_indexes(geo_mean_of_logs, log_reference_sample):
+    first_indeces = find_indeces(log_reference_sample, 0.3, 0.7)
+    second_indeces = find_indeces(geo_mean_of_logs, 0.05, 0.95)
+    intersec_indeces = np.intersect1d(first_indeces, second_indeces)
+    log_reference_sample = np.take(log_reference_sample, intersec_indeces)
+    geo_mean_of_logs = np.take(geo_mean_of_logs, intersec_indeces)
+
+    return geo_mean_of_logs, log_reference_sample
 
 
-def get_log_TMM_k(Y_k_1, Y_k_2, N_k_1, N_k_2):
-    log_TMM_k = 0
+def delete_zero_cells(reference, sample):
+    indexes = np.array([]).astype(int)
+    for i in range(sample.shape[0]):
+        if sample[i] == 0 and reference[i] == 0:
+            indexes = np.append(indexes, np.array(i))
 
-    for i in range(1, Y_k_1.shape[0]):
-        Y_g_k_1 = float(Y_k_1[4][i])
-        Y_g_k_2 = float(Y_k_2[4][i])
-        if Y_g_k_1 != 0 and Y_g_k_2 != 0:
-            log_TMM_k += (N_k_1 - Y_g_k_1) / (N_k_1 * Y_g_k_1) + (N_k_2 - Y_g_k_2) / (N_k_2 * Y_g_k_2)
+    sample = np.delete(sample, indexes)
+    reference = np.delete(reference, indexes)
 
-    return log_TMM_k
+    return reference, sample
 
 
-def run(input_file_1, input_file_2):
-    first_library_index = input_file_1.split(".")[1]
-    second_library_index = input_file_2.split(".")[1]
+def calculate_new_sample(new_library_size, sample):
+    sum = np.sum(sample)
+    new_sample = np.zeros((sample.shape[0]))
+    if sum != 0:
+        new_sample = (sample / np.sum(sample)) * new_library_size
+    return new_sample
 
-    print("Collecting files....")
-    df_file_1, df_file_2 = get_files(input_file_1, input_file_2)
 
-    Y_k_1 = df_file_1.iloc[:, 4:5]
-    N_k_1 = sum_vector(Y_k_1)
+def get_data(reference_library, sample):
+    reference_index = reference_library.file_index
+    reference = np.loadtxt(reference_library.file_name, dtype=str, delimiter="\t")[1:, 4].astype(np.float)
+    sample_index = sample.file_index
+    genes = np.loadtxt(sample.file_name, dtype=str, delimiter="\t")[1:, 0]
+    sample = np.loadtxt(sample.file_name, dtype=str, delimiter="\t")[1:, 4].astype(np.float)
+    return genes, reference, reference_index, sample, sample_index
 
-    Y_k_2 = df_file_2.iloc[:, 4:5]
-    N_k_2 = sum_vector(Y_k_2)
 
-    genes_ = df_file_2.iloc[:, 0:1]
-    genes = genes_.values.flatten()
+def run(input_dir):
+    get_reference = GetReference(input_dir)
+    reference_library, samples = get_reference.count_read_scaled_counts()
+    normalized_samples = np.zeros(28, samples.shape[0])
 
-    M_g_vector = get_M_g_v(N_k_1, N_k_2, Y_k_1, Y_k_2)
-    trimmed_M_g = trim(M_g_vector)
+    for i, sample in enumerate(samples):
+        if i <= 2:
+            genes, reference, reference_index, sample, sample_index = get_data(reference_library, sample)
+            reference, sample = delete_zero_cells(reference, sample)
 
-    sum_M_g_v_f = get_sum_M_g_v_f(trimmed_M_g)
-    log_TMM_k = get_log_TMM_k(Y_k_1, Y_k_2, N_k_1, N_k_2)
+            sample_sum = np.sum(sample)
+            log_reference_sample = np.zeros((reference.shape[0]))
+            if sample_sum != 0:
+                log_reference_sample = np.log(reference / (np.sum(reference)) / (sample / np.sum(sample)))
 
-    f_k = np.power(2, sum_M_g_v_f / log_TMM_k)
-    f_k_s_r = np.sqrt(f_k)
+            geo_mean_of_logs = (np.log(reference) + np.log(sample)) / 2
+            geo_mean_of_logs, log_reference_sample = get_vectors_at_indexes(geo_mean_of_logs, log_reference_sample)
 
-    N_k_1 = N_k_1 / f_k_s_r
-    N_k_2 = N_k_2 * f_k_s_r
+            scale_factor = calculate_weights_and_factor(log_reference_sample, geo_mean_of_logs)
+            new_library_size = np.sum(sample) * scale_factor
 
-    write_to_file(N_k_1, N_k_2, Y_k_1, Y_k_2, first_library_index, genes, second_library_index)
+            new_sample = calculate_new_sample(new_library_size, sample)
+            normalized_samples = np.append(normalized_samples, np.array(new_sample), axis=i)
+
+            write_to_file_two_conditions_one_repeat(reference, new_sample, genes, reference_index, sample_index,
+                                                    input_dir)
+
+            # import matplotlib.pyplot as plt
+            # plt.plot(new_sample, reference, 'bs')
+            # plt.title("Library{}_VS_Reference".format(sample_index))
+            # plt.xlim([0, 10000])
+            # plt.ylim([0, 10000])
+            # plt.ylabel("Reference_{}".format(reference_index))
+            # plt.xlabel("Library{}".format(sample_index))
+            # plt.show()
+            # plt.savefig(input_dir + "\\test\\" + "Library{}_VS_Reference".format(sample_index))
+
+    # write_to_file_two_conditions_one_repeat(input_dir, normalized_samples, genes)
+    print(1)
 
 
 if __name__ == '__main__':
-    run(sys.argv[1], sys.argv[2])
-
+    run(sys.argv[1])
